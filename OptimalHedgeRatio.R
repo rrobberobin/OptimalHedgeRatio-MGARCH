541bc8ed5fd21b837ba24b18f78b29eca0107f81
Robin Perälä <robin.perala@student.hanken.fi>
2021-10-26 14:07:56 +0300

    updated gitIgnore

--- OptimalHedgeRatio.R
+++ OptimalHedgeRatio.R
@@ -4,11 +4,15 @@ rm(list=ls)
 cat("\14")
 library(readxl)
 
-dataURL = ""
-
-read_excel(dataURL)
-
+dataURL = "Data/eMini.xlsx"
+data = read_excel(dataURL,skip=1)
+withDates = cbind(as.Date(data$Date,"%Y-%m-%d"),data[,-1])
+plot.ts(withDates[,2])
+plot.ts(data[,2])
 
 #OLS
+lm(data)
+
 
 #Garch
+
