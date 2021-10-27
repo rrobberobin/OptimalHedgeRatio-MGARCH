e953b8d28c3532d01ae87d0ba29c540222486f7f
Robin Perälä <robin.perala@student.hanken.fi>
2021-10-27 23:55:02 +0300

    Data cleaning. OLS created

--- OptimalHedgeRatio.R
+++ OptimalHedgeRatio.R
@@ -1,18 +1,45 @@
 #OptimalHedgeRatio
 
-rm(list=ls)
+rm(list=ls())
 cat("\14")
 library(readxl)
 
-dataURL = "Data/eMini.xlsx"
-data = read_excel(dataURL,skip=1)
-withDates = cbind(as.Date(data$Date,"%Y-%m-%d"),data[,-1])
-plot.ts(withDates[,2])
-plot.ts(data[,2])
+futuresURL = "Data/Weather.xlsx"
+fName = names(read_excel(futuresURL, n_max=1))  #extract the name of the dataset
+fData = read_excel(futuresURL, skip=2)
+
+underlyingURL = "Data/eMini.xlsx"
+uName = names(read_excel(underlyingURL, n_max=1))  #extract the name of the dataset
+uData = read_excel(underlyingURL, skip=2)
+
+merged = merge(uData, fData, by="Date")
+omitted = na.omit(merged[,c(1,4,9)])  #choose only specific rows
+
+library(xts)
+plot(xts(omitted[,2:3],omitted$Date,yaxis.same=F, multi.panel=T))
+plot(xts(omitted[,2],omitted$Date,yaxis.same=F, multi.panel=T))
+plot(xts(omitted[,3],omitted$Date,yaxis.same=F, multi.panel=T))
+change = lapply(as.vector(omitted[,2:3]), diff, lag=1)
+
+#diff(as.matrix(omitted[,3],lag=1))
+#uChange = diff(omitted$Price,lag=1)
+
+
 
 #OLS
-lm(data)
+plot(unlist(omitted[2]), unlist(omitted[3]))
+summary(lm(unlist(omitted[2]) ~ unlist(omitted[3])))
+
+plot(unlist(change[1]), unlist(change[2]))
+summary(lm(unlist(change[1]) ~ unlist(change[2])))
 
 
 #Garch
+
+
+
+
+
+
+
 
